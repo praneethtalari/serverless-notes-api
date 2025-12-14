f90faf1fad7613c21afa2e516dffd4e005a5d8b9
praneethtalari <48175181+praneethtalari@users.noreply.github.com>
2025-12-14 00:36:33 -0500

    Create iam.tf

---
+++ infra/iam.tf
@@ -0,0 +1,41 @@
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda-notes-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:Scan"
+        ],
+        Resource = aws_dynamodb_table.notes.arn
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
