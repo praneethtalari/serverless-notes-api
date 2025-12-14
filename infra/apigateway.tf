4d5f8944673c5f869cdd3872a6ce02cee17eaf41
praneethtalari <48175181+praneethtalari@users.noreply.github.com>
2025-12-14 00:42:13 -0500

    Create apigateway.tf

---
+++ infra/apigateway.tf
@@ -0,0 +1,36 @@
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "notes-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.notes_lambda.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "post_notes" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "POST /notes"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_route" "get_notes" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /notes"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "api_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.notes_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
+}
